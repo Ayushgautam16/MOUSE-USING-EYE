f2461aa4c9e3aebd3f81e88445ae36d1263e20d1
AYUSH GAUTAM <142661926+Ayushgautam16@users.noreply.github.com>
2024-07-18 00:38:18 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import cv2
+import mediapipe as mp
+import pyautogui
+
+# Initialize webcam and face mesh detector
+cam = cv2.VideoCapture(0)
+face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
+screen_w, screen_h = pyautogui.size()
+
+# Parameters to adjust sensitivity
+mouse_sensitivity = 1.5  # Adjust this value to change sensitivity
+
+while True:
+    _, frame = cam.read()
+    frame = cv2.flip(frame, 1)
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    output = face_mesh.process(rgb_frame)
+    landmark_points = output.multi_face_landmarks
+    frame_h, frame_w, _ = frame.shape
